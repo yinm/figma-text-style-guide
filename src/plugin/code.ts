24f6778d33ffce073decbfa95d2b2912822d3361
yinm <yinm.cminor9@gmail.com>
2024-06-23 16:56:54 +0900

    Implement sample app

--- src/plugin/code.ts
+++ src/plugin/code.ts
@@ -1,1 +1,16 @@
-console.log("sample");
+figma.showUI(__html__);
+
+figma.ui.onmessage = (msg: { count: number }) => {
+  const nodes: SceneNode[] = [];
+  for (let i = 0; i < msg.count; i++) {
+    const rect = figma.createRectangle();
+    rect.x = i * 150;
+    rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
+    figma.currentPage.appendChild(rect);
+    nodes.push(rect);
+  }
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
+
+  figma.closePlugin();
+};
