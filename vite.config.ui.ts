bcc97f47eb4ed01da86e97c8c99e11173846c734
yinm <yinm.cminor9@gmail.com>
2024-06-23 14:36:16 +0900

    Setup UI build

---
+++ vite.config.ui.ts
@@ -0,0 +1,12 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteSingleFile()],
+  root: "src/ui",
+  build: {
+    outDir: "dist",
+  },
+});
