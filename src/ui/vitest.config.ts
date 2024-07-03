2c828e0ab7662272e45cf8bd084fb646c8aeb023
yinm <yinm.cminor9@gmail.com>
2024-07-03 20:14:43 +0900

    Setup test config

---
+++ src/ui/vitest.config.ts
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    include: ["**/*.spec.ts", "**/*.spec.tsx"],
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
+});
