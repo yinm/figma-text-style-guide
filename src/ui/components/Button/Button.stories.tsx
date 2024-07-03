3ef0d3b158076b677fabf7ff2c11c7e582508794
yinm <yinm.cminor9@gmail.com>
2024-07-03 21:06:55 +0900

    Apply button styles

--- src/ui/components/Button/Button.stories.tsx
+++ src/ui/components/Button/Button.stories.tsx
@@ -1,3 +1,3 @@
 import { Button } from "./Button";
 
-export const Default = () => <Button>Hello</Button>;
+export const Default = () => <Button>Create</Button>;
--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,5 +1,10 @@
 import { PropsWithChildren } from "react";
 
 export const Button = ({ children }: PropsWithChildren) => (
-  <button type="button">{children}</button>
+  <button
+    type="button"
+    className="text-on-primary bg-primary rounded-lg px-4 py-2"
+  >
+    {children}
+  </button>
 );
