92aa0e41f3e67cd2689ffd8c0f27107bb43af23f
yinm <yinm.cminor9@gmail.com>
2024-07-04 21:22:13 +0900

    pnpm scaffdog

---
+++ src/plugin/nodes/createTextStyleFrame/createTextStyleFrame.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { createTextStyleFrame } from './createTextStyleFrame'
+
+describe("createTextStyleFrame", () => {
+  it("test", () => {
+    expect(1).toBe(1);
+  });
+})
