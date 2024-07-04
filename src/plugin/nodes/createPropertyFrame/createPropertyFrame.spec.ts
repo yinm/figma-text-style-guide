6c7fd7bf2a623598bf07ff6a16f3814be725df6a
yinm <yinm.cminor9@gmail.com>
2024-07-04 22:04:04 +0900

    pnpm scaffdog

---
+++ src/plugin/nodes/createPropertyFrame/createPropertyFrame.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { createPropertyFrame } from './createPropertyFrame'
+
+describe("createPropertyFrame", () => {
+  it("test", () => {
+    expect(1).toBe(1);
+  });
+});
