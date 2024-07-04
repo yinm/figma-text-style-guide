0f0421a7b5c4285651508abce623bec6ba815f94
yinm <yinm.cminor9@gmail.com>
2024-07-04 21:32:15 +0900

    pnpm scaffdog

---
+++ src/plugin/nodes/createTextStyleGuideFrame/createTextStyleGuideFrame.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { createTextStyleGuideFrame } from './createTextStyleGuideFrame'
+
+describe("createTextStyleGuideFrame", () => {
+  it("test", () => {
+    expect(1).toBe(1);
+  });
+});
