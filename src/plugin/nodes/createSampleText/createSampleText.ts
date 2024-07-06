5127e47a25544a33b79f6cadf9f5db2c35a7be66
yinm <yinm.cminor9@gmail.com>
2024-07-06 22:00:38 +0900

    Add a createSampleText function

--- src/plugin/nodes/createSampleText/createSampleText.ts
+++ src/plugin/nodes/createSampleText/createSampleText.ts
@@ -1,2 +1,29 @@
-export function createSampleText() {
+export async function createSampleText({
+  fontName,
+  id,
+  listSpacing,
+  hangingList,
+  hangingPunctuation,
+}: TextStyle) {
+  await figma.loadFontAsync({
+    family: fontName.family,
+    style: fontName.style,
+  });
+
+  const sampleText = figma.createText();
+  await sampleText.setTextStyleIdAsync(id);
+  sampleText.characters = `Lorem ipsum dolor sit amet, elit.
+aaaaaaa`;
+
+  if (listSpacing !== 0 || hangingList) {
+    sampleText.setRangeListOptions(0, sampleText.characters.length, {
+      type: "ORDERED",
+    });
+  }
+
+  if (hangingPunctuation) {
+    sampleText.characters = `"${sampleText.characters}`;
+  }
+
+  return sampleText;
 }
