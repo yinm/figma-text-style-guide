b488ab0d26f3f3d1d5863ff02c66aa3b15539eee
yinm <yinm.cminor9@gmail.com>
2024-07-06 16:05:16 +0900

    pnpm scaffdog

---
+++ src/plugin/nodes/createSpecFrame/createSpecFrame.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { createSpecFrame } from './createSpecFrame'
+
+describe("createSpecFrame", () => {
+  it("test", () => {
+    expect(1).toBe(1);
+  });
+});
