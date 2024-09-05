3980610cfa721bfa7e2904e549aa5ca58d3c53a7
yinm <yinm.cminor9@gmail.com>
2024-09-05 22:03:05 +0900

    Apply design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
+import {
+  BackgroundFillPrimary,
+  BackgroundFillSwitchThumb,
+  BackgroundFillSwitchTrackOff,
+  BackgroundPrimary,
+  TextLabel,
+  TextOnPrimary,
+} from "./design-tokens/dist/tokens.js";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["src/ui/**/*.tsx"],
   theme: {
     backgroundColor: {
-      primary: "#18a0fb",
-      "switch-thumb": "#ffffff",
-      "switch-track-off": "#808080",
+      primary: BackgroundPrimary,
+      "fill-primary": BackgroundFillPrimary,
+      "switch-thumb": BackgroundFillSwitchThumb,
+      "switch-track-off": BackgroundFillSwitchTrackOff,
     },
     textColor: {
-      "on-primary": "#ffffff",
-      label: "#ffffff",
+      label: TextLabel,
+      "on-primary": TextOnPrimary,
     },
     borderRadius: {
       lg: "0.5rem",
